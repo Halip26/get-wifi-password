bf4eff86993a729a7efe41a6b5a626f663031726
Halip26 <halipuddin.angko@gmail.com>
2023-09-13 21:14:32 +0800

    update the comments

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ def get_windows_saved_ssids():
     return ssids
 
 
+# Fungsi untuk mendapatkan Key wifi yang disimpan OS windows
 def get_windows_saved_wifi_passwords(verbose=1):
+    # Menjalankan command untuk mendapatkan rincian SSID
     ssids = get_windows_saved_ssids()
     Profile = namedtuple("Profile", ["ssid", "ciphers", "key"])
     profiles = []
@@ -27,36 +29,41 @@ def get_windows_saved_wifi_passwords(verbose=1):
         ssid_details = subprocess.check_output(
             f"""netsh wlan show profile "{ssid}" key=clear"""
         ).decode()
-        # mendapatkan sandinya
+        # Mendapatkan tipe enkripsi dari rincian SSID
         ciphers = re.findall(r"Cipher\s(.*)", ssid_details)
         # kosongkan spaces and colon
         ciphers = "/".join([c.strip().strip(":").strip() for c in ciphers])
         # mendapatkan Wi-Fi password
         key = re.findall(r"Key Content\s(.*)", ssid_details)
-        # kosongkan spaces and colon
+        # Menghapus spasi dan titik dua
         try:
             key = key[0].strip().strip(":").strip()
+
         except IndexError:
             key = "None"
+        # Membuat named tuple Profile untuk SSID yang sedang diproses
         profile = Profile(ssid=ssid, ciphers=ciphers, key=key)
+        # Menampilkan rincian SSID jika verbose >= 1
         if verbose >= 1:
             print_windows_profile(profile)
         profiles.append(profile)
     return profiles
 
 
+# Fungsi untuk menampilkan rincian SSID
 def print_windows_profile(profile):
-    """Mencetak satu profil di Windows"""
-    print(f"{profile.ssid:25}{profile.ciphers:15}{profile.key:50}")
+    """Mencetak lebih dari satu profil di Windows"""
+    print(f"{profile.ssid:32}{profile.ciphers:15}{profile.key:50}")
 
 
 def print_windows_profiles(verbose):
     """Mencetak semua SSID yang diekstraksi beserta Key di Windows"""
-    print("SSID                     CIPHER(S)      KEY")
-    print("-" * 50)
+    print("SSID                 \t\tCIPHER(S)      KEY")
+    print("-" * 60)
     get_windows_saved_wifi_passwords(verbose)
 
 
+# Fungsi untuk mendapatkan Key wifi yang disimpan OS Linux
 def get_linux_saved_wifi_passwords(verbose=1):
     network_connections_path = "/etc/NetworkManager/system-connections/"
     fields = ["ssid", "auth-alg", "key-mgmt", "psk"]
